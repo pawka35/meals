144fda45af36c31463e42883384b65a74a68c7a6
pavel.b@tlight.biz <pavel.b@tlight.biz>
2023-10-24 22:41:50 +0300

    24.10.2023

---
+++ lib/screens/filters.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/tabs.dart';
+import 'package:meals/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  _FiltersState createState() => _FiltersState();
+}
+
+class _FiltersState extends State<FiltersScreen> {
+  var _gluttenFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: (identifier) {
+          Navigator.of(context).pop();
+          if (identifier == 'meals') {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (ctx) => const TabsScreen(),
+            ));
+          }
+        },
+      ),
+      body: Column(children: [
+        SwitchListTile(
+          value: _gluttenFreeFilterSet,
+          onChanged: (isChecked) {
+            setState(() {
+              _gluttenFreeFilterSet = isChecked;
+            });
+          },
+          title: Text(
+            'Glutein-free',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          subtitle: Text(
+            'Only include gluten-free meals.',
+            style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        )
+      ]),
+    );
+  }
+}
