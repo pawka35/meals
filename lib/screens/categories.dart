8290dd944af3e85bbcd9daecd1dd2e061925fb68
pavel.b@tlight.biz <pavel.b@tlight.biz>
2023-10-29 18:25:57 +0300

    dimamic_data provider

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,11 +6,7 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {super.key,
-      required this.onToggleFavorite,
-      required this.availableMeals});
-  final void Function(Meal meal) onToggleFavorite;
+  const CategoriesScreen({super.key, required this.availableMeals});
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
@@ -25,7 +21,6 @@ class CategoriesScreen extends StatelessWidget {
           builder: (ctx) => MealsScreen(
             title: category.title,
             meals: filteredMeals,
-            onToggleFavorite: onToggleFavorite,
           ),
         ));
   }
